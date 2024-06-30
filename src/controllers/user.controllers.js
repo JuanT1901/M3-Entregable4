4f855daa6f25541c5a02b7417183443834ce5d34
Juan Torres <junete190@gmail.com>
2024-06-29 21:00:16 -0500

    Generación de token al hacer login

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 const catchError = require("../utils/catchError");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 const getAll = catchError(async (req, res) => {
   const results = await User.findAll();
@@ -43,6 +44,24 @@ const update = catchError(async (req, res) => {
   });
   if (result[0] === 0) return res.sendStatus(404);
   return res.json(result[1][0]);
+});
+
+const login = catchError(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ where: { email } });
+  if (!user) return res.status(401).json({ error: "Invalid credentials" });
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if(!isValid) return res.status(401).json({ error: "Invalid credentials" });
+
+  const token = jwt.sign(
+    {user},
+    process.env.TOKEN_SECRET,
+    {expiresIn: "1d"}
+  )
+
+  return res.json(user, token);
 });
 
 module.exports = {
