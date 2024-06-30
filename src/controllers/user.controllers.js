e57562988fb5ece9647f06be70a4fbcdab74326c
Juan Torres <junete190@gmail.com>
2024-06-29 21:17:06 -0500

    Obtención de usuario logueado

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -64,10 +64,17 @@ const login = catchError(async (req, res) => {
   return res.json(user, token);
 });
 
+const logged = catchError(async(req, res) => {
+  const user = req.user;
+  return res.json(user);
+})
+
 module.exports = {
   getAll,
   create,
   getOne,
   remove,
   update,
+  login,
+  logged,
 };
